0017f16edfdd362f05faf087afc8fa237126704e
JulianOcampoVelez <julian.ocampov@udea.edu.co>
2021-04-13 14:27:04 -0500

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+e
+5app/src/main/java/com/jovel/appchange/MainActivity.kt,7\4\74054b910fe07a8caa3bc55ff935dd1e8178647d
---
+++ app/src/main/java/com/jovel/appchange/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jovel.appchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
