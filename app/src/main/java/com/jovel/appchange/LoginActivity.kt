e2c3074f40960ce1ebcbf6d99a0722e3acde87cd
JnPabloS <Juanps2007@hotmail.com>
2021-04-18 11:28:41 -0500

    Cambio de icono y color

--- app/src/main/java/com/jovel/appchange/LoginActivity.kt
+++ app/src/main/java/com/jovel/appchange/LoginActivity.kt
@@ -3,9 +3,8 @@ package com.jovel.appchange
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Window
 import android.widget.Toast
-import androidx.core.view.isNotEmpty
 import com.jovel.appchange.databinding.ActivityLoginBinding
 
 private const val EMPTY = ""
@@ -22,31 +21,31 @@ class LoginActivity : AppCompatActivity() {
         val data = intent.extras
         val nombre = data?.getString("nombre")
         val correo = data?.getString("correo")
-        val contraseña = data?.getString("contraseña")
+        val contra = data?.getString("contraseña")
         val band = data?.getInt("band")
 
         if (band == 1){
-            loginBinding.emailEditText?.setText(correo)
-            loginBinding.passwordEditText?.setText(contraseña)
+            loginBinding.emailEditText.setText(correo)
+            loginBinding.passwordEditText.setText(contra)
         }
         else{
-            loginBinding.emailEditText?.setText(EMPTY)
-            loginBinding.passwordEditText?.setText(EMPTY)
+            loginBinding.emailEditText.setText(EMPTY)
+            loginBinding.passwordEditText.setText(EMPTY)
         }
 
         loginBinding.loginButton.setOnClickListener{
             if(loginBinding.emailEditText.text.toString().isNotEmpty() && loginBinding.passwordEditText.text.toString().isNotEmpty()){
-                if(loginBinding.emailEditText.text.toString() == correo && loginBinding.passwordEditText.text.toString() == contraseña){
+                if(loginBinding.emailEditText.text.toString() == correo && loginBinding.passwordEditText.text.toString() == contra){
                     val intent = Intent(this,MainActivity::class.java)                  //Se nombra "intent" haciendo referencia a "Intent", Intent funciona Intent(de donde estoy, para donde voy)
                     intent.putExtra("correo", correo)                                           //Envía la información
                     intent.putExtra("nombre", nombre)
-                    intent.putExtra("contraseña", contraseña)
+                    intent.putExtra("contraseña", contra)
                     startActivity(intent)                                                              //Ejecuta la sentencia
                     finish()
                 }
-                else Toast.makeText(this, "El usuario no existe", Toast.LENGTH_SHORT).show()
+                else Toast.makeText(this, getString(R.string.warning_login), Toast.LENGTH_SHORT).show()
             }
-            else Toast.makeText(this, "Algún campo está vacío", Toast.LENGTH_SHORT).show()
+            else Toast.makeText(this, getString(R.string.warning_login_empty), Toast.LENGTH_SHORT).show()
         }
 
         loginBinding.registerButton.setOnClickListener{
